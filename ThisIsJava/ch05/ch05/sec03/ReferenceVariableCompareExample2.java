07945ec86f9185535ff542b6127619eca8386ef5
lalajiji <hebbt@naver.com>
2022-10-06 22:26:50 +0900

    2022-10-06

---
+++ ThisIsJava/ch05/ch05/sec03/ReferenceVariableCompareExample2.java
@@ -0,0 +1,17 @@
+package ch05.sec03;
+
+public class ReferenceVariableCompareExample2 {
+
+	public static void main(String[] args) {
+		// int 타입의 배열 변수 arr1, arr2, arr3 변수 선언 
+		
+		// 배열 {1, 2, 3,} 을 생성하고 arr1 변수에 대입
+		// 배열 {1, 2, 3,} 을 생성하고 arr2 변수에 대입
+		// 배열 변수 arr2의 값을 배열 변수 arr3에 대입
+		
+		// arr1과 arr2 변수가 같은 배열을 참조하는지 검사
+		// arr2와 arr3 변수가 같은 배열을 참조하는지 검사
+
+	}
+
+}
