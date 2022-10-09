d13ee881e56d2f5bbce9ae908ba1a0b506c93e36
lalajiji <hebbt@naver.com>
2022-10-09 12:12:51 +0900

    2022-10-09

---
+++ ThisIsJava/ch08/ch08/sec04/RemoteControlExample4.java
@@ -0,0 +1,19 @@
+package ch08.sec04.c;
+
+public class RemoteControlExample4 {
+
+	public static void main(String[] args) {
+		RemoteControl rc = null;
+		rc = new Audio();
+		rc.turnOn();
+		rc.setVolume(20);
+		rc.turnOff();
+		
+		rc = new Television();
+		rc.turnOn();
+		rc.setVolume(5);
+		rc.turnOff();
+
+	}
+
+}
