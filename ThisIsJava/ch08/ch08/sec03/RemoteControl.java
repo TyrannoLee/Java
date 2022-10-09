7f40ccdec4b169b9ecce5ce7c02e43ce08e10d90
lalajiji <hebbt@naver.com>
2022-10-09 11:22:07 +0900

    2022-10-09

---
+++ ThisIsJava/ch08/ch08/sec03/RemoteControl.java
@@ -0,0 +1,8 @@
+package ch08.sec03;
+
+public interface RemoteControl {
+	//상수필드 앞에는 public static final이 생략되어있다.
+	int MAX_VOLUM=30;
+	int MIN_VOLUM=0;
+
+}
---
+++ ThisIsJava/ch08/ch08/sec03/RemoteControlExample.java
@@ -0,0 +1,13 @@
+package ch08.sec03;
+
+public class RemoteControlExample {
+
+	public static void main(String[] args) {
+		System.out.println("리모콘의 최대 볼륨: "+RemoteControl.MAX_VOLUM);
+		System.out.println("리모콘의 최대 볼륨: "+RemoteControl.MIN_VOLUM);
+		//상수는 구현 객체와 관련 없는 인터페이스 소속 멤버
+		//바로 접근해서 값을 읽을 수 있다.
+
+	}
+
+}
